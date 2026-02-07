7cbe919bcf9d5e18e512f0ea5f0d5b3e7a6b770a
Phil Olinger <philip.olinger@gmail.com>
2026-02-07 12:25:34 -0500

    Move get tx,ty,ta until after tv is valid.

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -124,11 +124,12 @@ RobotContainer::RobotContainer() {
 
   // Sample - display limelight stats to SmartDashboard
   std::shared_ptr<nt::NetworkTable> table = nt::NetworkTableInstance::GetDefault().GetTable("limelight");
-  tx = table->GetNumber("tx", 0.0);  // Horizontal offset from crosshair to target in degrees
-  ty = table->GetNumber("ty", 0.0);  // Vertical offset from crosshair to target in degrees
-  ta = table->GetNumber("ta", 0.0);  // Target area (0% to 100% of image)
   hasTarget = table->GetNumber("tv", 0.0); // Do you have a valid target (0=false, 1=true)?
   if (hasTarget == 1.0) {
+    tx = table->GetNumber("tx", 0.0);  // Horizontal offset from crosshair to target in degrees
+    ty = table->GetNumber("ty", 0.0);  // Vertical offset from crosshair to target in degrees
+    ta = table->GetNumber("ta", 0.0);  // Target area (0% to 100% of image)
+
     frc::SmartDashboard::PutNumber("Limelight/tx", tx);
     frc::SmartDashboard::PutNumber("Limelight/ty", ty);
     frc::SmartDashboard::PutNumber("Limelight/ta", ta);
