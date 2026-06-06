0f524be178d75f0068cd2c3d548ae237d49dbea8
GadgetGirls <frcteam4816@gmail.com>
2026-06-06 14:11:13 -0400

    Changed kMaxSpeed from 3mps to 1.5mps

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -126,8 +126,8 @@ constexpr units::ampere_t kTurningMotorCurrentLimit = 20_A;
 }  // namespace ModuleConstants
 
 namespace AutoConstants {
-constexpr auto kMaxSpeed = 3_mps;
-constexpr auto kMaxAcceleration = 3_mps_sq;
+constexpr auto kMaxSpeed = 1.5_mps;
+constexpr auto kMaxAcceleration = 1.5_mps_sq;
 constexpr auto kMaxAngularSpeed = 3.142_rad_per_s;
 constexpr auto kMaxAngularAcceleration = 3.142_rad_per_s_sq;
 
